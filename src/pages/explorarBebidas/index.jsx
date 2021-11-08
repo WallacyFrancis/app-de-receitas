6c70ec7cf98b2f08e71bfecfad2d125eef3deb72
Thyara <thyaranonato@outlook.com>
2021-11-08 17:42:30 -0300

    Requisitos 70 ao 74 - explorar bebidas ou comidas

--- src/pages/explorarBebidas/index.jsx
+++ src/pages/explorarBebidas/index.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
-function ExplorarBebidas() {
+function ExplorarBebidas({ history }) {
   return (
     <>
       <Header title="Explorar Bebidas" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/bebidas/178319') }
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </>
   );
 }
+
+ExplorarBebidas.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default ExplorarBebidas;
