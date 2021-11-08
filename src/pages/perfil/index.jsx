6c7a45ed1340d608a218a14caedd61945c6da181
Thyara <thyaranonato@outlook.com>
2021-11-08 17:54:03 -0300

    Requisitos 82 ao 87 - perfil

--- src/pages/perfil/index.jsx
+++ src/pages/perfil/index.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
-function Perfil() {
+function Perfil({ history }) {
+  function getEmail() {
+    if (localStorage.getItem('user')) {
+      const { email } = JSON.parse(localStorage.getItem('user'));
+      return email;
+    } return '';
+  }
+
   return (
     <>
       <Header title="Perfil" />
+      <h3 data-testid="profile-email">{ getEmail() }</h3>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Receitas Feitas
+      </button>
+
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => history.push('/receitas-favoritas') }
+
+      >
+        Receitas Favoritas
+      </button>
+
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ () => {
+          history.push('/');
+          localStorage.clear();
+        } }
+      >
+        Sair
+      </button>
       <Footer />
     </>
   );
 }
+
+Perfil.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Perfil;
