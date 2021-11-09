d4cb7a6209520fba9f295e9d432a520b0ba4a3cb
Wallacy Francis <wallacy_francis@hotmail.com>
2021-11-09 19:28:29 -0300

    Resolvido requisitos 14 e 15

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,10 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
+import {
+  fetchMealsName,
+  fetchMealsIngredient,
+  fetchMealsFirstLetter,
+  fetchDrinksName,
+  fetchDrinksIngredient,
+  fetchDrinksFirstLetter,
+} from '../services/fetchAPI';
+
 function Provider({ children }) {
+  const [recipes, setRecipes] = useState([]);
+  const [redirect, setRedirect] useState
+  const history = useHistory();
+
+  function showAlert(recipesResult) {
+    if (recipesResult === null) {
+      global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    } else {
+      setRecipes(recipesResult);
+    }
+  }
+
+  async function searchMeals(searchInputText, radioFilter) {
+    let recipesResult = [];
+    if (radioFilter === 'ingredient') {
+      recipesResult = await fetchMealsIngredient(searchInputText);
+      console.log(recipesResult);
+      showAlert(recipesResult);
+    }
+    if (radioFilter === 'name') {
+      recipesResult = await fetchMealsName(searchInputText);
+      console.log(recipesResult);
+      showAlert(recipesResult);
+    }
+    if (radioFilter === 'firstLetter') {
+      if (searchInputText.length > 1) {
+        return global.alert('Sua busca deve conter somente 1 (um) caracter');
+      }
+      recipesResult = await fetchMealsFirstLetter(searchInputText);
+      console.log(recipesResult);
+      showAlert(recipesResult);
+    }
+    if (recipesResult !== null && recipesResult.length === 1) {
+      const id = recipesResult[0].idMeal;
+      // history.push(`/comidas/${id}`);
+      return (<Redirect to={ `/comidas/${id}` } />);
+      // console.log(recipesResult);
+      // console.log(id);
+      // Arrabiata
+    }
+  }
+
+  async function searchDrinks(searchInputText, radioFilter) {
+    let recipesResult = [];
+    if (radioFilter === 'ingredient') {
+      recipesResult = await fetchDrinksIngredient(searchInputText);
+      console.log(recipesResult);
+      showAlert(recipesResult);
+    }
+    if (radioFilter === 'name') {
+      recipesResult = await fetchDrinksName(searchInputText);
+      console.log(recipesResult);
+      showAlert(recipesResult);
+    }
+    if (radioFilter === 'firstLetter') {
+      if (searchInputText.length > 1) {
+        return global.alert('Sua busca deve conter somente 1 (um) caracter');
+      }
+      recipesResult = await fetchDrinksFirstLetter(searchInputText);
+      console.log(recipesResult);
+      showAlert(recipesResult);
+    }
+    if (recipesResult !== null && recipesResult.length === 1) {
+      const { idDrink } = recipesResult;
+      history.push(`/bebidas/${idDrink}`);
+    }
+  }
+
+  function searchRecipes(inputs, pathname) {
+    const { searchInputText, radioFilter } = inputs;
+    if (pathname.includes('comidas')) {
+      searchMeals(searchInputText, radioFilter);
+    }
+    if (pathname.includes('bebidas')) {
+      searchDrinks(searchInputText, radioFilter);
+    }
+  }
+
+  const contextValue = {
+    recipes,
+    searchRecipes,
+  };
+
   return (
-    <Context.Provider>
+    <Context.Provider value={ contextValue }>
       { children }
     </Context.Provider>
   );
