579e6bb470e95b51bd36fd1425a9c19ac462f6e9
Wallacy Francis <wallacy_francis@hotmail.com>
2021-11-16 20:33:46 -0300

    Resolvido requisitos 35, 75 e 76

--- src/pages/detalhesBebidas/index.jsx
+++ src/pages/detalhesBebidas/index.jsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import PropTypes from 'prop-types';
 import RecomendationMealsCard from '../../components/RecomendationMealCard';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+import shareIcon from '../../images/shareIcon.svg';
 
 function DetalhesBebidas({ history }) {
   const [drink, setDrink] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
   const id = useHistory().location.pathname.split('/')[2];
 
   useEffect(() => { // requisição drinks pelo id
@@ -12,9 +16,55 @@ function DetalhesBebidas({ history }) {
       const { drinks } = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
         .then((response) => response.json());
       setDrink(drinks[0]);
+      console.log(drinks[0]);
     }
     getDrinksById();
   }, [id]);
+
+  function getIngredientsAndMeasures() {
+    let ingredients = [];
+    const NUMBER_OF_INGREDIENTS = 20;
+    for (let index = 1; index <= NUMBER_OF_INGREDIENTS; index += 1) {
+      const srtIngred = `strIngredient${index}`;
+      const strMeasur = `strMeasure${index}`;
+      const IngredAndMeasur = `${drink[srtIngred]} - ${drink[strMeasur]}`;
+      ingredients = [...ingredients, IngredAndMeasur];
+    }
+    const finalRecipe = ingredients.filter((ingredient) => (
+      ingredient !== ' -  '
+      && ingredient !== ' - '
+      && ingredient !== 'null - null'
+      && ingredient !== 'undefined - undefined'
+    ));
+    return finalRecipe;
+  }
+
+  const ingredients = getIngredientsAndMeasures();
+
+  function saveLocalStorage(object) {
+    console.log(object);
+    // const arrFavorite = [...JSON.parse(localStorage.getItem('favoriteRecipes')), object];
+    // console.log(arrFavorite);
+    // localStorage.setItem('favoriteRecipes', JSON.stringify(object));
+  }
+
+  function favoriteClick() {
+    if (isFavorite) {
+      setIsFavorite(false);
+    } else {
+      setIsFavorite(true);
+      const objDrink = {
+        id: drink.idDrink,
+        type: 'drink',
+        area: '',
+        category: drink.strCategory,
+        alcoholicOrNot: drink.strAlcoholic,
+        name: drink.strDrink,
+        image: drink.strDrinkThumb,
+      };
+      saveLocalStorage(objDrink);
+    }
+  } // strArea
 
   return (
     <div>
@@ -25,16 +75,41 @@ function DetalhesBebidas({ history }) {
         width="200px"
       />
       <h3 data-testid="recipe-title">{ drink.strDrink }</h3>
-      <button type="button" data-testid="share-btn">Compartilhar</button>
-      <button type="button" data-testid="favorite-btn">Favoritar</button>
+      <button type="button" data-testid="share-btn">
+        <img src={ shareIcon } alt="share" />
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+        onClick={ () => favoriteClick() }
+      >
+        {
+          !isFavorite ? (<img src={ whiteHeartIcon } alt="Favorite" />)
+            : (<img src={ blackHeartIcon } alt="Non favorite" />)
+        }
+      </button>
+      <br />
       <span data-testid="recipe-category">{ drink.strAlcoholic }</span>
+      <br />
       <span data-testid="0-ingredient-name-and-measure">
         Ingredientes:
         <ul>
-          { }
+          { ingredients
+            .map((ingredient, index) => (
+              <li
+                key={ index }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {ingredient}
+              </li>))}
         </ul>
       </span>
       <span data-testid="instructions">{ drink.strInstructions }</span>
+      <br />
+      <video data-testid="video" src={ drink.srtYoutube } controls width="400">
+        <track kind="captions" />
+        Seu navegador não suporta o elemento
+      </video>
       <br />
       <p>
         Receitas Recomendadas
